7454fec9f4232892762073fb8cfc95742c8379b9
bgani <ganibdev@gmail.com>
2021-02-03 23:51:38 +0600

    adding revit api & get current document

---
+++ RevitPlugin/ViewAndFilterElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+
+
+namespace RevitPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ViewAndFilterElements : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                // current doc
+                Document document = commandData.Application.ActiveUIDocument.Document;
+
+                return Result.Succeeded;
+            }
+
+            catch (Exception ex)
+            {
+                return Result.Failed;
+            }
+        }
+    }
+}
